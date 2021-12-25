0622099349d39c7bc76d506809d106295eb3179c
sapirshm <sapirsh1995@gmail.com>
2021-12-25 18:55:24 +0200

    assignment9

--- flaskProject2/app.py
+++ flaskProject2/app.py
@@ -1,8 +1,15 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request,session
 
 
 app = Flask(__name__)
+app.secret_key='123'
 
+users = {'user1': {'name': 'Yossi', 'email': 'yo@gmail.com'},
+         'user2': {'name': 'Sapir', 'email': 'sapir@gmail.com'},
+         'user3': {'name': 'Mika', 'email': 'mika@gmail.com'},
+         'user4': {'name': 'Inbar', 'email': 'inbar@gmail.com'},
+         'user5': {'name': 'Aviv', 'email': 'aviv@gmail.com'},
+         }
 
 @app.route("/")
 @app.route("/CV")
@@ -12,10 +19,35 @@ def cv_page():
 
 @app.route("/assignment8")
 def assignment8_page():
-    name = 'Sapir'
-    last_name = 'Shmuelevitz'
+    name = ''
+    last_name = ''
     return render_template('Assignment8.html',
                            name=name,
                            last_name=last_name,
                            profile={'Hobbie1': 'Dance',
                                    'Hobbie2': 'Hockey'})
+
+
+
+@app.route("/assignment9", methods=['GET', 'POST'])
+def assignment9_page():
+    # search form
+    if 'email' in request.args:
+        email = request.args['email']
+        if email == '':
+            return render_template('assignment9.html', user_list=users)
+        # search it in users dict
+        for key, value in users.items():
+            if value.get('email') == email:
+                return render_template('assignment9.html', u_name=value.get('name'), u_email=value.get('email'))
+    # registration form
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        session['username'] = username
+    return render_template('assignment9.html')
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout_func():
+    session['username'] = ''
+    return render_template('assignment9.html')
