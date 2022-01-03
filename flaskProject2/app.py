a0cc02f290978f7533136e2a7343726c02ed80a0
sapirshm <sapirsh1995@gmail.com>
2022-01-03 19:39:21 +0200

    Assignment10FinalNew

--- flaskProject2/app.py
+++ flaskProject2/app.py
@@ -52,57 +52,8 @@ def logout_func():
     session['username'] = ''
     return render_template('assignment9.html')
 
-
-alert_change = ""
-
-assignment10 = Blueprint('assignment10', __name__, static_folder='static', static_url_path='/', template_folder='templates')
-
-@app.route("/assignment10", methods=['GET', 'POST'])
-def assignment10_page():
-    global  alert_change
-    alert_change = ""
-    return render_template('assignment10.html')
-
-
+from pages.assignment10.assignment10 import assignment10
 app.register_blueprint(assignment10)
 
-# insert
-@app.route('/insert_user', methods=['POST'])
-def insert_user_func():
-    name = request.form['name']
-    email = request.form['email']
-    query = "INSERT INTO users(name, email) VALUES ('%s', '%s')" % (name, email)
-    interact_db(query=query, query_type='commit')
-    global  alert_change
-    alert_change = "The user "+name+" is inserted"
-    return redirect('/user_list')
-
-# update
-@app.route('/update_user', methods=['POST'])
-def update_user_func():
-    name = request.form['name']
-    new_email = request.form['new_email']
-    query = "update users set email = '%s' where name = '%s'" % (new_email, name)
-    interact_db(query=query, query_type='commit')
-    global alert_change
-    alert_change = "The email of the user "+name+" is updated"
-    return redirect('/user_list')
-
-# delete
-@app.route('/delete_user', methods=['POST'])
-def delete_user_func():
-    name = request.form['name']
-    query = "DELETE FROM users WHERE name='%s'" % name
-    interact_db(query, query_type='commit')
-    global  alert_change
-    alert_change = "The user "+name+" was deleted"
-    return redirect('/user_list')
-
-# display
-@app.route('/user_list')
-def user_list_func():
-    query = "select * from users"
-    query_result = interact_db(query=query, query_type='fetch')
-    return render_template('assignment10.html', user_list=query_result,  alert_change= alert_change)
 if __name__ == '__main__':
     app.run(debug=True)
